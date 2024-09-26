77d9138093dd5068680814cdf780651bef6cc552
lima2206 <vitor.lima2206@gmail.com>
2024-09-26 04:14:33 -0300

    Template Electron + Typescript + NextJs

---
+++ KeyMaster/main/background.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+;(async () => {
+  await app.whenReady()
+
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    show: false
+  })
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/home`)
+
+    //Descomente para abrir automaticamente o DevTools
+    // mainWindow.webContents.openDevTools()
+  }
+
+  //isso aqui garante que só vai abrir o app quando estiver carregado
+  mainWindow.on('ready-to-show', () => mainWindow.show())
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('message', async (event, arg) => {
+  event.reply('message', `${arg} World!`)
+})
