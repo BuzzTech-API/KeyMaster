6c9dda8614a92ee5b23fca707990930334d865e5
IvanDuarte <ivangermanoduarte@gmail.com>
2024-10-17 20:59:01 -0300

    feat: Adiciona mensagem de status de conexão com banco de dados ao backend.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(private dataSource: DataSource) {}
+
+  async onModuleInit(){
+    await this.checkDatabaseConnection();
+  }
+
+  async checkDatabaseConnection() {
+    try {
+        const isConnected = this.dataSource.isInitialized;
+        if (isConnected) {
+            console.log('Database connection is OK!');
+        } else {
+            await this.dataSource.initialize(); // Opcional para tentar reconectar, se não estiver conectado
+            console.log('Database reconnected successfully!');
+        }
+    } catch (error) {
+        console.error('Database connection failed:', error.message);
+        console.error('Error Details:', error);
+    }
+  }
   getHello(): string {
     return 'Hello World!';
   }
