3d26c29b268810cb63be26b3f22e5a4b7224e9ab
IvanDuarte <ivangermanoduarte@gmail.com>
2024-11-24 21:50:33 -0300

    feat: Adiciona função para criar log de deleções do sistema, medida de segurança que prioriza a redundancia na estratégia de sanitização dos backUps

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -25,9 +25,7 @@ export class User {
     @Column()
     userKey: string
 
-    @OneToMany(() => Password, (password) => password.user)
+    @OneToMany(() => Password, (password) => password.user, { onDelete: 'CASCADE'})
     passwords: Password[];
-
-
 
 }
