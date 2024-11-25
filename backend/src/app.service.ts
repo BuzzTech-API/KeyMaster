cf392a281ba07efa259b346ef11ff8a515d55776
IvanDuarte <ivangermanoduarte@gmail.com>
2024-11-24 23:50:02 -0300

    feat: Adiciona a implementação da função de sanitização de backup logo ao inicializar o backEnd, garantindo assim a sanitização automática do sistema.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,17 +1,24 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { UserService } from './user/services/user.service';
 
 @Injectable()
 export class AppService implements OnModuleInit {
   // Ivan Germano: DataSource é uma classe que identifica conexões de Banco de Dados
-  constructor(private dataSource: DataSource) {}
+  constructor(
+    private dataSource: DataSource, // Ivan Germano: Injeta o serviço de blacklist
+    private userService: UserService // Ivan Germano: Injeta o serviço de usuário
+  ) {}
 
   // Ivan Germano: Essa função determina de forma arbitrária que o modulo só continuará com as demais rotinas
   // após realizar essa função primeiro.
   async onModuleInit() {
     console.log('Iniciando o módulo principal...');
     await this.checkDatabaseConnection();
+    
+    // Ivan Germano: Chamar a sanitização dos dados restaurados assim que o módulo for inicializado
+    await this.userService.sanitizeRestoredData();
   }
 
   // Ivan Germano: Essa função tem responsabilidade de verificar se a conexão com o Banco de Dados Postgres está funcionando.
